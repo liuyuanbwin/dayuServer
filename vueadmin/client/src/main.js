f77d3067e6bc3f0eec70c3f9a2bd6e4ba8a8287f
liuyuanbwin <liuyuanbwin@163.com>
2019-07-06 23:41:57 +0800

    o

--- vueadmin/client/src/main.js
+++ vueadmin/client/src/main.js
@@ -27,7 +27,7 @@ VueAMap.initAMapApiLoader({
 });
 
 Vue.prototype.$axios = axios
-axios.defaults.baseURL = 'http://localhost:3000'
+axios.defaults.baseURL = 'http://127.0.0.1:3000'
 
 //全局时间格式化过滤
 Vue.filter('moment', function (value, formatString) {
