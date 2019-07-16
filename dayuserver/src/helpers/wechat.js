e3c6b757b2d02c06ae72fd579367393d991e8cbd
LeoLau <liuyuansix@163.com>
2019-07-16 17:18:32 +0800

    modify

--- dayuserver/src/helpers/wechat.js
+++ dayuserver/src/helpers/wechat.js
@@ -1,5 +1,5 @@
 const baseUrl = "https://api.weixin.qq.com/"
-const config = require('../../src/config')
+const config = require('./config')
 const WxApi = {
     accessToken: baseUrl+"cgi-bin/token?grant_type=client_credential"
 }
