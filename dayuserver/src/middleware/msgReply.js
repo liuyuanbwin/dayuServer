110df304983ac076aee68e40f0c6ab3c99fbfbb1
LeoLau <liuyuansix@163.com>
2019-07-17 14:57:44 +0800

    modify

--- dayuserver/src/middleware/msgReply.js
+++ dayuserver/src/middleware/msgReply.js
@@ -1,39 +1,31 @@
-const xmlt = require('../helpers/xml')
+const xml = require('../helpers/xml')
+const Vehicle = require('../models/vehicle')
 module.exports = () => {
     return async (ctx, next) => {
-        console.log('====================================');
-        console.log(ctx.req.body);
-        console.log('====================================');
-        // ctx.body = {
-        //     msg:"success"
-        // }
-        // console.log('====================================');
-        var rxml = ctx.req.body.xml
-        var msgType = rxml.MsgType[0]
-        var content = rxml.Content[0]
-        var toUserName = rxml.ToUserName[0]
-        var toFromName = rxml.FromUserName[0]
+
+        var msg = ctx.req.body.xml
+        var msgType = msg.MsgType[0]
+        var content = msg.Content[0]
+        var toUserName = msg.ToUserName[0]
+        var toFromName = msg.FromUserName[0]
         const createTime = Date.parse(new Date())
 
-        // console.log(`<xml>
-        // <ToUserName><![CDATA[${toFromName}]]></ToUserName>
-        // <FromUserName><![CDATA[${toUserName}]]></FromUserName>
-        // <CreateTime>${createTime}</CreateTime>
-        // <MsgType><![CDATA[text]]></MsgType>
-        // <Content><![CDATA[欢迎关注陈小皮公众号，下面请开始你的表演！]]></Content>
-        // </xml>`)
+        
+       
+        const result = await Vehicle.findOne({
+            plate_num:{$regex:content}
+        })
 
-        ctx.body = xmlt.jsonToXml({
+
+        ctx.body = xml.jsonToXml({
             xml: {
                 ToUserName: toFromName,
                 FromUserName: toUserName,
                 CreateTime: Date.now(),
                 MsgType: msgType,
-                Content: content + "skfjahksjfh "
+                Content: result + "skfjahksjfh "
             }
         })
-       
-
 
        // await next()
     }
