9f0e3eda97a77e3dac431770578c94979321ad6e
LeoLau <liuyuansix@163.com>
2019-07-19 16:36:27 +0800

    sss

--- dayuserver/src/middleware/msgReply.js
+++ dayuserver/src/middleware/msgReply.js
@@ -7,40 +7,47 @@ module.exports = () => {
 
         var msg = ctx.req.body.xml
         var msgType = msg.MsgType[0]
-        var content = msg.Content[0]
-        var toUserName = msg.ToUserName[0]
-        var toFromName = msg.FromUserName[0]
-        const createTime = Date.parse(new Date())
 
-        const result = await Vehicle.findOne({
-            plate_num:{$regex:content}
-        })
 
-        Dayu.postModelMsg({
-            touser:toFromName,
-            template_id:'IIFWNAzKrk_ZXpR92NA3D-VQoBDGVIsyDAQRKpCjVJc',
-            topcolor:"#FF0000",
-            data:{
-                first:{
-                    value:'尊敬的' + result.plate_num +'车主您的交强险信息如下',
-                    color:"#778899"
-                },
-                keynote1:{
-                    value:moment(result.cli_expire_date).format('YYYY-MM-DD'),
-                    color:'#005500'
-                },
-                keynote2:{
-                    value:moment(result.cli_expire_date).format('YYYY-MM-DD'),
-                    color:'#000077'
-                },
-                remark:{
-                    value:'大宇车友竭诚为您服务,详情联系石微微',
-                    color:'#777700'
+        if(msgType == 'text'){
+            var content = msg.Content[0]
+            var toUserName = msg.ToUserName[0]
+            var toFromName = msg.FromUserName[0]
+            const createTime = Date.parse(new Date())
+    
+            const result = await Vehicle.findOne({
+                plate_num:{$regex:content}
+            })
+
+            console.log('查询结果' -- > JSON.stringify(result)) 
+    
+            Dayu.postModelMsg({
+                touser:toFromName,
+                template_id:'IIFWNAzKrk_ZXpR92NA3D-VQoBDGVIsyDAQRKpCjVJc',
+                topcolor:"#FF0000",
+                data:{
+                    first:{
+                        value:'尊敬的' + result.plate_num +'车主您的交强险信息如下',
+                        color:"#778899"
+                    },
+                    keynote1:{
+                        value:moment(result.cli_expire_date).format('YYYY-MM-DD'),
+                        color:'#005500'
+                    },
+                    keynote2:{
+                        value:moment(result.cli_expire_date).format('YYYY-MM-DD'),
+                        color:'#000077'
+                    },
+                    remark:{
+                        value:'大宇车友竭诚为您服务,详情联系石微微',
+                        color:'#777700'
+                    }
                 }
-            }
-        }).then(res => {
-            console.log('模板消息返回 --> ' + JSON.stringify(res))
-        })
+            }).then(res => {
+                console.log('模板消息返回 --> ' + JSON.stringify(res))
+            })
+        }
+       
 
         ctx.body = 'success'
 
