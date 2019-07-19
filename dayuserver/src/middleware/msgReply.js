306fc152ef5c13544c72a4b5c7d8724e2e60c99e
LeoLau <liuyuansix@163.com>
2019-07-19 16:42:09 +0800

    sss

--- dayuserver/src/middleware/msgReply.js
+++ dayuserver/src/middleware/msgReply.js
@@ -20,32 +20,41 @@ module.exports = () => {
             })
 
             console.log('查询结果 -- >' + JSON.stringify(result)) 
-    
-            Dayu.postModelMsg({
-                touser:toFromName,
-                template_id:'IIFWNAzKrk_ZXpR92NA3D-VQoBDGVIsyDAQRKpCjVJc',
-                topcolor:"#FF0000",
-                data:{
-                    first:{
-                        value:'尊敬的' + result.plate_num +'车主您的交强险信息如下',
-                        color:"#778899"
-                    },
-                    keynote1:{
-                        value:moment(result.cli_expire_date).format('YYYY-MM-DD'),
-                        color:'#005500'
-                    },
-                    keynote2:{
-                        value:moment(result.cli_expire_date).format('YYYY-MM-DD'),
-                        color:'#000077'
-                    },
-                    remark:{
-                        value:'大宇车友竭诚为您服务,详情联系石微微',
-                        color:'#777700'
+
+
+            if(result == null){
+                ctx.body ="未查询到您的车辆信息,请确确认后重新查询.发送车牌号码查询车辆投保信息,字母为大写"
+            }else{
+                await Dayu.postModelMsg({
+                    touser:toFromName,
+                    template_id:'IIFWNAzKrk_ZXpR92NA3D-VQoBDGVIsyDAQRKpCjVJc',
+                    topcolor:"#FF0000",
+                    data:{
+                        first:{
+                            value:'尊敬的' + result.plate_num +'车主您的交强险信息如下',
+                            color:"#778899"
+                        },
+                        keynote1:{
+                            value:moment(result.cli_expire_date).format('YYYY-MM-DD'),
+                            color:'#005500'
+                        },
+                        keynote2:{
+                            value:moment(result.cli_expire_date).format('YYYY-MM-DD'),
+                            color:'#000077'
+                        },
+                        remark:{
+                            value:'大宇车友竭诚为您服务,详情联系石微微',
+                            color:'#777700'
+                        }
                     }
-                }
-            }).then(res => {
-                
-            })
+                }).then(res => {
+                    
+                })
+
+                ctx.body = 'success' 
+            }
+    
+            
         }
        
 
