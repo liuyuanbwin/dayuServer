968b8067760d13f045f68a8943d572f364ae21cd
LeoLau <liuyuansix@163.com>
2019-07-25 15:52:42 +0800

    order

--- dayuserver/src/routes/api/clients.js
+++ dayuserver/src/routes/api/clients.js
@@ -1,4 +1,5 @@
 const router = require('koa-router')()
+const Client = require('../../models/client')
 const User = require('../../models/user')
 const Dayu = require('../../helpers/dayu')
 
@@ -9,6 +10,22 @@ router.get('/', async (ctx, next) => {
                 // .then((res, req) => {
                 //     console.log('userlist ' + JSON.stringify(res))
                 // })
+    var openids = result.body.data.openid 
+    var i = 0
+    openids.forEach(async element => {
+        const client = new Client({
+            openid:element
+        })
+
+        try {
+            await client.save()
+            console.log(' count ' + i)
+            i ++ 
+        } catch (err) {
+            console.log('err ' + err )
+        }
+    });
+
 
     ctx.body = JSON.stringify(result)
 })
