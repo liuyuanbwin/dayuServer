48571247997c64ffb42e3c20137dd02671b493ac
liuyuanbwin <liuyuanbwin@163.com>
2019-08-24 17:35:27 +0800

    ssss

--- vueadmin/client/src/dayu/modules/menu/Menu.vue
+++ vueadmin/client/src/dayu/modules/menu/Menu.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <el-button type="primary" @click="queryMenu()">查询菜单</el-button>
-  <el-button type="success">成功按钮</el-button>
+  <el-button type="success" @click="createMenu()">成功按钮</el-button>
   <el-button type="info">信息按钮</el-button>
     </div>
 </template>
@@ -21,6 +21,14 @@
                 .get('/api/menu')
                 .then(res => {
                     console.log('菜单' + JSON.stringify(res))
+                })
+            },
+            createMenu(){
+                this
+                .$axios
+                .post('/api/menu/create')
+                .then(res => {
+                    console.log('新建菜单 ' + JSON.stringify(res))
                 })
             }
         }
