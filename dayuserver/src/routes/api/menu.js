9e89cd6b2772dbf203e5c1a328549cbfe3f78a15
liuyuanbwin <liuyuanbwin@163.com>
2019-08-24 17:12:45 +0800

    ssss

--- dayuserver/src/routes/api/menu.js
+++ dayuserver/src/routes/api/menu.js
@@ -6,6 +6,7 @@ router.prefix('/api/menu')
 router.get('/', async(ctx, next) => {
     let token = await Token.getToken('token')
     let result = await Dayu.getMenuInfo(token.token)
+    console.log('menu ---> ' + JSON.stringify(result))
     ctx.body = result
 })
 
