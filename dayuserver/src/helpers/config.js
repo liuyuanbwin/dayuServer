469ca064696da6b4bd72a2cf345453afc87868b8
LeoLau <liuyuansix@163.com>
2019-09-11 14:00:17 +0800

    ssss

--- dayuserver/src/helpers/config.js
+++ dayuserver/src/helpers/config.js
@@ -5,7 +5,7 @@ module.exports = {
     wx: {
         token: 'leolau',
         appid: 'wxf5a4243b7b09ffc0',
-        appSecret: 'a4e42b620c5b78027cf44cffd1aabb76',
+        appSecret: 'e7a40e19b62f4bb7a5cf81202508846c',
         encodingAESKey: ''
     }
 }
