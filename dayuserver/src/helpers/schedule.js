f98425c53df2cde8676022c23fc301af4e9a75db
LeoLau <liuyuansix@163.com>
2019-09-11 14:46:17 +0800

    ssss

--- dayuserver/src/helpers/schedule.js
+++ dayuserver/src/helpers/schedule.js
@@ -26,9 +26,9 @@ exports.schedule = () => {
         new schedule.Range(1, 6)
     ];
 
-    rule.hour =7;
+    rule.hour =15;
 
-    rule.minute =0;
+    rule.minute =50;
     var j = schedule.scheduleJob(rule, async function () {
 
         console.log('schedule sssss')
@@ -38,11 +38,12 @@ exports.schedule = () => {
 
         console.log('clenit ' + JSON.stringify(clients))
 
-        //clients.body.data.openid =  ["omkUruH6_g0dovrbmjMM5VdtHAe4","omkUruLvdfDSS51akLyGDs9CV2CA", "omkUruLhonCbGOw9ywJWzZ3vJUg0"]
+        clients.body.data.openid =  ["omkUruH6_g0dovrbmjMM5VdtHAe4","omkUruLvdfDSS51akLyGDs9CV2CA", "omkUruLhonCbGOw9ywJWzZ3vJUg0"]
         console.log('dirname ' + __dirname)
         var myDate = new Date();
         var mytime = myDate.toLocaleTimeString();
 
+        console.log(`用来请求的token是 ${JSON.stringify(token)} `+JSON.stringify(clients) + '\n')
         fs.appendFile(
             __dirname + '/log.txt',
             mytime + `用来请求的token是 ${JSON.stringify(token)} `+JSON.stringify(clients) + '\n',
@@ -57,43 +58,43 @@ exports.schedule = () => {
             .openid
             .forEach(async element => {
                 console.log('openid ' + element)
-                // try {
-                //     await Dayu.postModelMsg(token.token, {
-                //         touser: element,
-                //         template_id: 'ef-7cKV2Asjckz-WyvNTAn0a5CE0zMKiTVYk__OGUiE',
-                //         topcolor: "#FF0000",
-                //         url: "http://www.bl1000.cn/wx/weather",
-                //         data: {
-                //             first: {
-                //                 value: '本轮涿州限行如下',
-                //                 color: "#778899"
-                //             },
-                //             keyword1: {
-                //                 value: moment(new Date()).format('YYYY-MM-DD'),
-                //                 color: '#005500'
-                //             },
-                //             keyword2: {
-                //                 value: '涿州市二环路内（含二环路)',
-                //                 color: '#005500'
-                //             },
-                //             keyword3: {
-                //                 value: forbiddenStr,
-                //                 color: '#FF0000'
-                //             },
-                //             keyword4: {
-                //                 value: '07:00-19:00',
-                //                 color: '#005500'
-                //             },
-                //             remark: {
-                //                 value: '限行详情及近期天气请点击\"详情\"',
-                //                 color: '#778899'
-                //             }
-                //         }
-                //     });
-                // } catch (err) {
-                //     console.log('error ' + err);
+                try {
+                    await Dayu.postModelMsg(token.token, {
+                        touser: element,
+                        template_id: 'ef-7cKV2Asjckz-WyvNTAn0a5CE0zMKiTVYk__OGUiE',
+                        topcolor: "#FF0000",
+                        url: "http://www.bl1000.cn/wx/weather",
+                        data: {
+                            first: {
+                                value: '本轮涿州限行如下',
+                                color: "#778899"
+                            },
+                            keyword1: {
+                                value: moment(new Date()).format('YYYY-MM-DD'),
+                                color: '#005500'
+                            },
+                            keyword2: {
+                                value: '涿州市二环路内（含二环路)',
+                                color: '#005500'
+                            },
+                            keyword3: {
+                                value: forbiddenStr,
+                                color: '#FF0000'
+                            },
+                            keyword4: {
+                                value: '07:00-19:00',
+                                color: '#005500'
+                            },
+                            remark: {
+                                value: '限行详情及近期天气请点击\"详情\"',
+                                color: '#778899'
+                            }
+                        }
+                    });
+                } catch (err) {
+                    console.log('error ' + err);
 
-                // }
+                }
 
             })
     })
