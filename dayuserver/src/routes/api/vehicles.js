c68d3a4d8ffac8583324c664b92ce131261dee47
liuyuanbwin <liuyuansix@163.com>
2019-11-05 15:11:04 +0800

    Update vehicles.js

--- dayuserver/src/routes/api/vehicles.js
+++ dayuserver/src/routes/api/vehicles.js
@@ -37,6 +37,7 @@ router.get('/', async(ctx,next) => {
             
            
         }else{
+            console.log(`车牌号 ${ctx.params.plate_num}`)
             const result = await Vehicle.find({
                 plate_num:ctx.params.plate_num
             })
